36982a4bd3340c310c4ec34996c4c5acebca800b
gru6 <iliagrushevich@gmail.com>
2023-03-18 00:58:16 +0300

    feat: add local storage functional

--- react/src/components/Search.tsx
+++ react/src/components/Search.tsx
@@ -1,20 +1,50 @@
-export default function Search() {
-  return (
-    <>
-      <form id="search-form" role="search">
-        <input
-          id="q"
-          aria-label="Search contacts"
-          placeholder="Search"
-          type="search"
-          name="q"
-        />
-        <div id="search-spinner" aria-hidden hidden={true} />
-        <div className="sr-only" aria-live="polite"></div>
-      </form>
-      <form method="post">
-        <button type="submit">New</button>
-      </form>
-    </>
-  );
+import React from "react";
+
+type MyProps = { inputValue: string };
+type MyState = { inputValue: string };
+
+export default class Search extends React.Component<MyProps, MyState> {
+  constructor(props: MyProps) {
+    super(props);
+    this.state = {
+      inputValue: "",
+    };
+    this.addLocalStorage = this.addLocalStorage.bind(this);
+  }
+  addLocalStorage() {
+    const data = this.state.inputValue;
+    localStorage.setItem("myInputValue", JSON.stringify(data));
+  }
+  getLocalStorage() {
+    const data = String(localStorage.getItem("myInputValue"));
+    this.setState({
+      inputValue: JSON.parse(data),
+    });
+  }
+  //для доп вычислений перед итоговым рендорингом
+  /*   componentDidMount():void {
+    this.setState({ inputValue: this.getLocalStorage() });
+  } */
+
+  /*   componentWillUnmount() {
+    clearInterval(this.timerID);
+  } */
+
+  render() {
+    return (
+      <>
+        <form id="search-form" role="search">
+          <input
+            placeholder="Search"
+            type="search"
+            value={this.state.inputValue}
+            onChange={(e) => this.setState({ inputValue: e.target.value })}
+          />
+        </form>
+        <form>
+          <button onClick={this.addLocalStorage}>Search</button>
+        </form>
+      </>
+    );
+  }
 }
