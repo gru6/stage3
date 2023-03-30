b5433a89bdea09dbc7becbec895e919b175df731
gru6 <iliagrushevich@gmail.com>
2023-03-31 01:24:46 +0300

    refactor: rewrite Modal and Form component on Hooks

--- react/src/components/form/Form.tsx
+++ react/src/components/form/Form.tsx
@@ -1,3 +1,171 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import { useForm } from "react-hook-form";
+
+type Props = {
+  updateData: (data: Icards) => void;
+};
+
+export interface Icards {
+  text: string;
+  date: string;
+  sex: string;
+  box: string;
+  contact: string;
+  file: string;
+}
+
+export interface Istate {
+  cards: Icards[];
+  card: Icards;
+  isModalOpen: boolean;
+}
+
+export function Form(props: Props) {
+  const { register, handleSubmit, reset } = useForm<Icards>();
+
+  const [isModalOpen, setModalStatus] = useState(false);
+  /*   const [isChecked, setIsChecked] = useState(false)  Починить потом очистку чекбокса */
+  /*   const [cards, setCardsdArray] = useState([]);
+   */
+
+  function handleOpenModal() {
+    setModalStatus(true);
+  }
+
+  function handleCloseModal() {
+    setModalStatus(false);
+  }
+
+  const onSubmit = (data: Icards) => {
+    console.log("data :>> ", data);
+    const file = data.file?.[0];
+    if (file) {
+      const blob = new Blob([file], { type: "application/pdf" });
+      data.file = URL.createObjectURL(blob);
+    }
+    handleOpenModal();
+    handleDataForCard(data);
+    handleClearForm();
+  };
+
+  const handleClearForm = () => {
+    reset({
+      text: "",
+      date: "",
+      sex: "",
+      box: "",
+      contact: "",
+      file: "",
+    });
+  };
+
+  function handleDataForCard(data: Icards) {
+    props.updateData(data);
+  }
+
+  return (
+    <>
+      <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Person name:
+          <input
+            type="text"
+            /*   defaultValue={"Loe Choe"} */
+            {...register("text")}
+            required
+            pattern="^[a-zA-Zа-яА-Я]{2,50}[\s][a-zA-Zа-яА-Я]{2,50}$"
+            title="The name must contain 2 words longer than 2 letters"
+          />
+        </label>
+        <label>
+          Birthday:
+          <input
+            type="date"
+            /*  defaultValue={"2023-12-12"} */
+            {...register("date")}
+            required
+          />
+        </label>
+        <label htmlFor="sex">
+          Choose a sex:
+          <select required {...register("sex")} defaultValue="">
+            <option disabled value="">
+              -- Do not chosen --
+            </option>
+            <option value="Man">Man</option>
+            <option value="Woman">Woman</option>
+            <option value="Another">Another</option>
+          </select>
+        </label>
+        <label htmlFor="agreement">
+          I agree
+          <input
+            {...register("box")}
+            /*  defaultChecked */
+            type="checkbox"
+            name="agreement"
+            value="agreed"
+            required
+          />
+        </label>
+        <div className="radio-container">
+          <p>Please select your preferred contact method:</p>
+          <div className="radio-item">
+            <label htmlFor="contactChoice1">
+              Email
+              <input
+                {...register("contact")}
+                /*    defaultChecked */
+                required
+                type="radio"
+                id="contactChoice1"
+                name="contact"
+                value="email"
+              />
+            </label>
+            <label htmlFor="contactChoice2">
+              Phone
+              <input
+                {...register("contact")}
+                type="radio"
+                id="contactChoice2"
+                name="contact"
+                value="phone"
+              />
+            </label>
+            <label htmlFor="contactChoice3">
+              Telegram
+              <input
+                {...register("contact")}
+                type="radio"
+                id="contactChoice3"
+                name="contact"
+                value="telegram"
+              />
+            </label>
+          </div>
+        </div>
+
+        <label>
+          Upload avatar:
+          <input
+            type="file"
+            {...register("file")}
+            required
+            id="imageFile"
+            accept="image/*"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+
+      <Modal isOpen={isModalOpen} onClose={() => handleCloseModal()}></Modal>
+    </>
+  );
+}
+
+/* 
 import React, { Component, FormEvent, RefObject } from "react";
 import Modal from "./Modal";
 
@@ -109,6 +277,7 @@ export class Form extends Component<Props, Istate> {
             Person name:
             <input
               type="text"
+              defaultValue={"Loe Choe"}
               ref={this.textInput}
               required
               pattern="^[a-zA-Zа-яА-Я]{2,50}[\s][a-zA-Zа-яА-Я]{2,50}$"
@@ -117,12 +286,17 @@ export class Form extends Component<Props, Istate> {
           </label>
           <label>
             Birthday:
-            <input type="date" ref={this.dateInput} required />
+            <input
+              type="date"
+              defaultValue={"2023-12-12"}
+              ref={this.dateInput}
+              required
+            />
           </label>
           <label htmlFor="sex">
             Choose a sex:
             <select name="sex" required ref={this.selectInput} defaultValue="">
-              <option disabled value="">
+              <option disabled value="Man">
                 -- Do not chosen --
               </option>
               <option value="Man">Man</option>
@@ -134,6 +308,7 @@ export class Form extends Component<Props, Istate> {
             I agree
             <input
               ref={this.boxInput}
+              defaultChecked
               type="checkbox"
               name="agreement"
               value="agreed"
@@ -147,6 +322,7 @@ export class Form extends Component<Props, Istate> {
                 Email
                 <input
                   ref={this.contactInput}
+                  defaultChecked
                   required
                   type="radio"
                   id="contactChoice1"
@@ -195,9 +371,6 @@ export class Form extends Component<Props, Istate> {
     );
   }
 }
-
-/* 
-
 
 
 switcher (radio)
--- react/src/pages/FormPage.tsx
+++ react/src/pages/FormPage.tsx
@@ -1,46 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Icards } from "../components/form/Form";
 
 export interface Istate {
   cards: Icards[];
 }
 
-export default class FormPage extends React.Component<
-  Record<string, never>,
-  Istate
-> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    this.state = { cards: [] };
-    this.updateStateFormComponent = this.updateStateFormComponent.bind(this);
-  }
+export default function FormPage() {
+  const [cards, setCards] = useState<Icards[]>([]);
 
-  updateStateFormComponent(value: Icards) {
-    this.setState((prevState) => ({
-      cards: [...prevState.cards, value],
-    }));
-  }
+  const updateStateFormComponent = (value: Icards) => {
+    setCards([...cards, value]);
+  };
 
-  render() {
-    return (
-      <>
-        <Form updateData={this.updateStateFormComponent} />
-        <ul className="new-card-container">
-          {this.state.cards.map((card, index) => (
-            <div key={index} className="new-card">
-              New Card
-              <div>Person name: {card.text}</div>
-              <div>Birthday: {card.date}</div>
-              <div>Sex: {card.sex}</div>
-              <div>Client agree with everything</div>
-              <div>Contact: {card.contact}</div>
-              <div className="img-ccontainer">
-                Avatar: <img src={card.file} alt="" className="new-card-img" />
-              </div>
+  return (
+    <>
+      <Form updateData={updateStateFormComponent} />
+      <ul className="new-card-container">
+        {cards.map((card, index) => (
+          <div key={index} className="new-card">
+            New Card
+            <div>Person name: {card.text}</div>
+            <div>Birthday: {card.date}</div>
+            <div>Sex: {card.sex}</div>
+            <div>Client agree with everything</div>
+            <div>Contact: {card.contact}</div>
+            <div className="img-ccontainer">
+              Avatar: <img src={card.file} alt="" className="new-card-img" />
             </div>
-          ))}
-        </ul>
-      </>
-    );
-  }
+          </div>
+        ))}
+      </ul>
+    </>
+  );
 }
